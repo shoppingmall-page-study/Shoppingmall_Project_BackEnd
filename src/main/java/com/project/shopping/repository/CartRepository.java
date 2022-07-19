8f9f45e9b44dc9bc1e2e672c0ed71942259e01f7
dongbok Lee <typoon0820@gmail.com>
2022-07-19 09:16:24 +0900

    Revert "Develop"

--- src/main/java/com/project/shopping/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.shopping.repository;
-
-import com.project.shopping.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart,Integer> {
-
-}
--- src/main/java/com/project/shopping/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.shopping.repository;
-
-import com.project.shopping.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review,Integer> {
-}
