5f3872a9995e81266546ec667dab9ce60ce70f4b
limjongheok <jjongbbang2@naver.com>
2022-08-12 00:26:09 +0900

    cors

--- src/main/java/com/project/shopping/config/WebMvcConfig.java
+++ src/main/java/com/project/shopping/config/WebMvcConfig.java
@@ -12,6 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://222.118.103.229")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("Access-Control-Allow-origin","*")
                 .allowCredentials(true)
