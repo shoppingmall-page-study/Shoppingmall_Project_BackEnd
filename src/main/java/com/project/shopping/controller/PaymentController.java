ab2e1acf7aa2a0d964108d092f22c51af612887a
dongbok Lee <typoon0820@gmail.com>
2022-09-12 16:03:33 +0900

    Update PaymentController.java

--- src/main/java/com/project/shopping/controller/PaymentController.java
+++ src/main/java/com/project/shopping/controller/PaymentController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Map; 
 
 @RestController
 @RequiredArgsConstructor
