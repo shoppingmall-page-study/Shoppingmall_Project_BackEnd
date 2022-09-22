36900210db677f6f152ff9fd137432cdf17c797c
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2022-09-22 18:33:25 +0900

    Update WebMvcConfig.java

--- src/main/java/com/project/shopping/config/WebMvcConfig.java
+++ src/main/java/com/project/shopping/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://222.118.103.229:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("Access-Control-Allow-origin","*")
                 .allowCredentials(true)
