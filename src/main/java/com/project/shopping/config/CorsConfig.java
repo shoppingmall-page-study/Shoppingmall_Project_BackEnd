13e93cc3175792388ead55d0a54cf471c4a1e665
limjonghyeok <42922673+limjongheok@users.noreply.github.com>
2022-09-25 20:58:21 +0900

    Update CorsConfig.java

--- src/main/java/com/project/shopping/config/CorsConfig.java
+++ src/main/java/com/project/shopping/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 내 서버가 응답을 할시 json을 자바스크립트에서 처리할 수 있게 할지 결정
         config.addAllowedOrigin("http://222.118.103.229:3000");// 모등 ip 에 응답 허용
+        config.addAllowedOrigin("http://222.118.103.229:3001");
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://222.118.103.229:80");
         config.addAllowedHeader("*"); // 모든 header 응답 허용
