b01a1c859c25d6806cc49d5c08a6c3fdd2cd9a20
limjongheok <jjongbbang2@naver.com>
2022-11-14 19:02:01 +0900

    img

--- src/main/java/com/project/shopping/dto/requestDTO/ProductRequestDTO/ProductCreateRequestDTO.java
+++ src/main/java/com/project/shopping/dto/requestDTO/ProductRequestDTO/ProductCreateRequestDTO.java
@@ -14,15 +14,16 @@ public class ProductCreateRequestDTO {
     private  String name;
     private  long price;
     private  int total;
-   // private String imgUrl;
+    private String imgUrl;
 
     @Builder
 
-    public ProductCreateRequestDTO(String title, String content, String name, long price, int total) {
+    public ProductCreateRequestDTO(String title, String content, String name, long price, int total, String imgUrl) {
         this.title = title;
         this.content = content;
         this.name = name;
         this.price = price;
         this.total = total;
+        this.imgUrl = imgUrl;
     }
 }
