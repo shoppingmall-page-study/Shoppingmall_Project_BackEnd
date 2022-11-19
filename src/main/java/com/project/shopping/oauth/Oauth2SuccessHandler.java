dc99831865b964ab3bc27affb70273c46ec1b332
limjongheok <jjongbbang2@naver.com>
2022-11-19 17:48:01 +0900

    oauth2 수정

--- src/main/java/com/project/shopping/oauth/Oauth2SuccessHandler.java
+++ src/main/java/com/project/shopping/oauth/Oauth2SuccessHandler.java
@@ -85,12 +85,12 @@ public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
     }
 
     private String makeRediretjoinUrl(String token) {
-        return UriComponentsBuilder.fromUriString("http://222.118.103.229/registration/"+token)
+        return UriComponentsBuilder.fromUriString("https://hannam.shop/registration/"+token)
                 .build().toUriString();
     }
 
     private String makeRedirectUrl(String jwttoken) {
-        return UriComponentsBuilder.fromUriString("http://222.118.103.229/oauth/"+jwttoken)
+        return UriComponentsBuilder.fromUriString("https://hannam.shop/oauth/"+jwttoken)
                 .build().toUriString();
     }
 
