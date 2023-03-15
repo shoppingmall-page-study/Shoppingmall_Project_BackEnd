46ea0a24d5ca7ed22b9bde33deaaba4fc9b6c4af
Dongbok-Lee <typoon0820@gmail.com>
2023-03-15 11:38:17 +0900

    리뷰관련 도메인 authentication 수정

--- src/main/java/com/project/shopping/controller/ReviewController.java
+++ src/main/java/com/project/shopping/controller/ReviewController.java
@@ -3,9 +3,11 @@ package com.project.shopping.controller;
 
 import com.project.shopping.Error.CustomException;
 import com.project.shopping.Error.ErrorCode;
+import com.project.shopping.auth.PrincipalDetails;
 import com.project.shopping.dto.requestDTO.RevieRequestDTO.ReviewCreateRequestDTO;
 import com.project.shopping.dto.requestDTO.RevieRequestDTO.ReviewUpdateRequestDTO;
 import com.project.shopping.dto.responseDTO.ReviewResponseDTO.*;
+import com.project.shopping.model.User;
 import com.project.shopping.service.ProductService;
 import com.project.shopping.service.ReviewService;
 import com.project.shopping.service.UserService;
@@ -31,7 +33,10 @@ public class ReviewController {
     @PostMapping("/api/review/create/{id}")
     public ResponseEntity<?> createReview(Authentication authentication, @PathVariable(value = "id") int ProductId, @RequestBody @Valid ReviewCreateRequestDTO reviewCreateRequestDTO){
 
-        ReviewCreateResponseDTO reviewCreateResponseDTO = reviewService.create(authentication, ProductId, reviewCreateRequestDTO);
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        User user = principalDetails.getUser();
+
+        ReviewCreateResponseDTO reviewCreateResponseDTO = reviewService.create(user, ProductId, reviewCreateRequestDTO);
 
         // Review Dto 로 응답 보내기
 
@@ -46,7 +51,10 @@ public class ReviewController {
     @DeleteMapping ("/api/review/delete/{id}")
     public ResponseEntity<?> reviewDelete(Authentication authentication, @PathVariable(value = "id") int ReviewId){ // reviewid
 
-        ReviewDeleteResponseDTO reviewDeleteResponseDTO = reviewService.deleteReview(authentication, ReviewId);
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        User user = principalDetails.getUser();
+
+        ReviewDeleteResponseDTO reviewDeleteResponseDTO = reviewService.deleteReview(user, ReviewId);
 
         Map<String , Object> result = new HashMap<>();
         result.put("msg","리뷰 삭제에 성공했습니다.");
@@ -58,10 +66,11 @@ public class ReviewController {
     @GetMapping("/api/review/user")
     public ResponseEntity<?> findUserReviewList(Authentication authentication){
         // 현재 로그인한 유저 찾기
-
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        User user = principalDetails.getUser();
 
         // 현재 나의 리뷰 목록 조회
-        List<ReviewUserJoinResponseDTO>  userReviewlist = reviewService.getEqUserAndActive(authentication,ActiveStatus);
+        List<ReviewUserJoinResponseDTO>  userReviewlist = reviewService.getEqUserAndActive(user,ActiveStatus);
 
         Map<String , Object> result = new HashMap<>();
         result.put("msg","리뷰 조회에 성공했습니다.");
@@ -87,9 +96,12 @@ public class ReviewController {
     }
 
     @PutMapping("/api/review/update/{id}")
-    public ResponseEntity<?> productUpdate(Authentication authentication, @PathVariable(value = "id") int reviewId, @RequestBody @Valid ReviewUpdateRequestDTO reviewUpdateRequestDTO){
+    public ResponseEntity<?> productReviewUpdate(Authentication authentication, @PathVariable(value = "id") int reviewId, @RequestBody @Valid ReviewUpdateRequestDTO reviewUpdateRequestDTO){
 
-        ReviewUpdateResponseDTO reviewUpdateResponseDTO = reviewService.update(authentication, reviewId, reviewUpdateRequestDTO);
+        PrincipalDetails principalDetails =  (PrincipalDetails) authentication.getPrincipal();
+        User user = principalDetails.getUser();
+
+        ReviewUpdateResponseDTO reviewUpdateResponseDTO = reviewService.update(user, reviewId, reviewUpdateRequestDTO);
 
         Map<String, Object> result = new HashMap<>();
         result.put("msg","리뷰수정에 성공했습니다.");
