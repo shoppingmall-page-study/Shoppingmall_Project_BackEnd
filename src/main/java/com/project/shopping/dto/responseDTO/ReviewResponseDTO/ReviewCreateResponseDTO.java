47dfdbe137b27c214a3481656671d229fbcf4769
limjongheok <jjongbbang2@naver.com>
2023-04-05 17:52:28 +0900

    4개 이하 생성자 변경

--- src/main/java/com/project/shopping/dto/responseDTO/ReviewResponseDTO/ReviewCreateResponseDTO.java
+++ src/main/java/com/project/shopping/dto/responseDTO/ReviewResponseDTO/ReviewCreateResponseDTO.java
@@ -1,31 +1,23 @@
 package com.project.shopping.dto.responseDTO.ReviewResponseDTO;
 
 import com.project.shopping.model.Review;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
+@AllArgsConstructor
 public class ReviewCreateResponseDTO {
     private int reviewId;
     private  String title;
     private  String content;
 
-    @Builder
-    public ReviewCreateResponseDTO(int reviewId, String title, String content) {
-        this.reviewId = reviewId;
-        this.title = title;
-        this.content = content;
-    }
 
 
     public static  ReviewCreateResponseDTO toReviewCreateResponseDTO(Review review){
-        return ReviewCreateResponseDTO.builder()
-                .reviewId(review.getId())
-                .title(review.getTitle())
-                .content(review.getContent())
-                .build();
+        return new ReviewCreateResponseDTO(review.getId(),review.getTitle(),review.getContent());
     }
 
 }
